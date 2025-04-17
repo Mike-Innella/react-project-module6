e4b42d32a0eb7d011dec8f5af453bb07c813b80a
Michael Innella <mainnella@gmail.com>
2025-04-17 16:32:45 -0400

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 // src/index.js
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/index.css";
+import App from "./App"; // Your main app component
+import "./styles/index.css"; // Your CSS file
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Main app component */}
   </React.StrictMode>
 );
